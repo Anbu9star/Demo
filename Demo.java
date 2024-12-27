d687d745cb16af2ce162dd0e79a2e7b4e1bf3201
Anbu-U <anbu.u@ninestar.co.in>
2024-12-27 10:35:25 +0530

    constructor and method added

--- Demo.java
+++ Demo.java
@@ -1,5 +1,13 @@
 class Demo{
+	Demo(){
+		System.out.println("This from constructor");
+	}
 	public static void main(String args[]){
-		System.out.println("This from java");
+		System.out.println("This from Main Method");
+		new Demo();
+		m1();
+	}
+	static void m1(){
+		System.out.println("Hi, i am a method");
 	}
 }
