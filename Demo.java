a260effbee49b4ac625752687cccad17a8112a7f
Anbu9star <anbu.u@ninestar.co.in>
2024-12-27 12:03:58 +0530

    Revert "constructor and method added"

--- Demo.java
+++ Demo.java
@@ -1,13 +1,5 @@
 class Demo{
-	Demo(){
-		System.out.println("This from constructor");
-	}
 	public static void main(String args[]){
-		System.out.println("This from Main Method");
-		new Demo();
-		m1();
-	}
-	static void m1(){
-		System.out.println("Hi, i am a method");
+		System.out.println("This from java");
 	}
 }
